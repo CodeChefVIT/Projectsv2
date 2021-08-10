235cf3a9176592edadd3eb70684c4b15e38db48c
Sahas <sahasvivek001@gmail.com>
2021-08-10 17:00:51 +0530

    ft.frontend structure

---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Testing the frontend!</h1>
+    </div>
+  )
+}
