d58f4d975e7f0fe53be2b218be3c3e66fa30f629
VisheshBansal <vishesh.bansal@gmail.com>
2021-08-22 23:25:19 +0530

    feat: added footer

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,8 @@
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
