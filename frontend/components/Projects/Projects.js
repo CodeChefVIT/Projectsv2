1575a4dbc89081856c0c8ef94e2696365ea1d08e
Sahas <sahasvivek001@gmail.com>
2021-08-24 01:27:15 +0530

    ft. Card layout added

---
+++ frontend/components/Projects/Projects.js
@@ -0,0 +1,105 @@
+// import ourProj from '../Projects/Projects.module.css'
+
+const Project = () => {
+    return (
+        <section className="projects bg-black">
+            <h2 className="text-yellow-400 sm:text-center lg:text-left lg:mx-16 text-2xl">2021</h2>
+            <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center">
+
+                <div className="my-10 mx-8">
+                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
+                        <img src="https://images.pexels.com/photos/1374295/pexels-photo-1374295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="img" className="w-100" />
+                        <div className="px-6 py-4">
+                            <div className="text-white mb-2 text-2xl font-bold px-2">FFDS</div>
+                            <p className="text-white mb-3 px-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit mollitia et officiis ut vero reiciendis voluptatum iusto. Perspiciatis cupiditate aut non quos.</p>
+                        </div>
+                        <div className="grid gap-5 mt-2 mb-6 justify-items-center">
+                            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
+                                View Project
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="my-10 mx-8">
+                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
+                        <img src="https://images.pexels.com/photos/1374295/pexels-photo-1374295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="img" className="w-100" />
+                        <div className="px-6 py-4">
+                            <div className="text-white mb-2 text-2xl font-bold px-2">FFDS</div>
+                            <p className="text-white mb-3 px-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit mollitia et officiis ut vero reiciendis voluptatum iusto. Perspiciatis cupiditate aut non quos.</p>
+                        </div>
+                        <div className="grid gap-5 mt-2 mb-6 justify-items-center">
+                            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
+                                View Project
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="my-10 mx-8">
+                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
+                        <img src="https://images.pexels.com/photos/1374295/pexels-photo-1374295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="img" className="w-100" />
+                        <div className="px-6 py-4">
+                            <div className="text-white mb-2 text-2xl font-bold px-2">FFDS</div>
+                            <p className="text-white mb-3 px-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit mollitia et officiis ut vero reiciendis voluptatum iusto. Perspiciatis cupiditate aut non quos.</p>
+                        </div>
+                        <div className="grid gap-5 mt-2 mb-6 justify-items-center">
+                            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
+                                View Project
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h2 className="text-yellow-400 sm:text-center lg:text-left lg:mx-16 text-2xl">2020</h2>
+            <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center">
+
+                <div className="my-10 mx-8">
+                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
+                        <img src="https://images.pexels.com/photos/1374295/pexels-photo-1374295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="img" className="w-100" />
+                        <div className="px-6 py-4">
+                            <div className="text-white mb-2 text-2xl font-bold px-2">FFDS</div>
+                            <p className="text-white mb-3 px-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit mollitia et officiis ut vero reiciendis voluptatum iusto. Perspiciatis cupiditate aut non quos.</p>
+                        </div>
+                        <div className="grid gap-5 mt-2 mb-6 justify-items-center">
+                            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
+                                View Project
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="my-10 mx-8">
+                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
+                        <img src="https://images.pexels.com/photos/1374295/pexels-photo-1374295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="img" className="w-100" />
+                        <div className="px-6 py-4">
+                            <div className="text-white mb-2 text-2xl font-bold px-2">FFDS</div>
+                            <p className="text-white mb-3 px-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit mollitia et officiis ut vero reiciendis voluptatum iusto. Perspiciatis cupiditate aut non quos.</p>
+                        </div>
+                        <div className="grid gap-5 mt-2 mb-6 justify-items-center">
+                            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
+                                View Project
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="my-10 mx-8">
+                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
+                        <img src="https://images.pexels.com/photos/1374295/pexels-photo-1374295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="img" className="w-100" />
+                        <div className="px-6 py-4">
+                            <div className="text-white mb-2 text-2xl font-bold px-2">FFDS</div>
+                            <p className="text-white mb-3 px-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit mollitia et officiis ut vero reiciendis voluptatum iusto. Perspiciatis cupiditate aut non quos.</p>
+                        </div>
+                        <div className="grid gap-5 mt-2 mb-6 justify-items-center">
+                            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
+                                View Project
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    );
+}
+
+export default Project;
