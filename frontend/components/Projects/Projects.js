bcfa0e0b21a4bff7600318e0cc18c91cf1678d96
Sahas <sahasvivek001@gmail.com>
2021-08-29 15:00:07 +0530

    ft.API calls and modal overlay

--- frontend/components/Projects/Projects.js
+++ frontend/components/Projects/Projects.js
@@ -1,17 +1,48 @@
-// import ourProj from '../Projects/Projects.module.css'
+import axios from "axios";
+import React from "react";
+// import Card from "./Card.js";
 
-const Project = () => {
+const baseURL = "https://projectsv2.herokuapp.com/project/get_data";
+let i;
+
+export default function App() {
+    const [post, setPost] = React.useState(null);
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setPost(response.data);
+        });
+    }, []);
+
+    if (!post) return null;
+
+    console.log(post);
     return (
         <section className="projects bg-black">
             <h2 className="text-yellow-400 sm:text-center lg:text-left lg:mx-16 text-2xl">2021</h2>
             <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center">
 
+
                 <div className="my-10 mx-8">
                     <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
-                        <img src="https://images.pexels.com/photos/1374295/pexels-photo-1374295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="img" className="w-100" />
+                        <img src="" alt="img" className="w-100" />
                         <div className="px-6 py-4">
-                            <div className="text-white mb-2 text-2xl font-bold px-2">FFDS</div>
-                            <p className="text-white mb-3 px-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit mollitia et officiis ut vero reiciendis voluptatum iusto. Perspiciatis cupiditate aut non quos.</p>
+                            <div className="text-white mb-2 text-2xl font-bold px-2">{post[0].repo_name}</div>
+                            <p className="text-white mb-3 px-2">{post[0].repo_description}</p>
+                        </div>
+                        <div className="grid gap-5 mt-2 mb-6 justify-items-center">
+                            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
+                                View Project
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="my-10 mx-8">
+                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
+                        <img src="" alt="img" className="w-100" />
+                        <div className="px-6 py-4">
+                            <div className="text-white mb-2 text-2xl font-bold px-2">{post[1].repo_name}</div>
+                            <p className="text-white mb-3 px-2">{post[1].repo_description}</p>
                         </div>
                         <div className="grid gap-5 mt-2 mb-6 justify-items-center">
                             <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
@@ -21,85 +52,8 @@ const Project = () => {
                     </div>
                 </div>
 
-                <div className="my-10 mx-8">
-                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
-                        <img src="https://images.pexels.com/photos/1374295/pexels-photo-1374295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="img" className="w-100" />
-                        <div className="px-6 py-4">
-                            <div className="text-white mb-2 text-2xl font-bold px-2">FFDS</div>
-                            <p className="text-white mb-3 px-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit mollitia et officiis ut vero reiciendis voluptatum iusto. Perspiciatis cupiditate aut non quos.</p>
-                        </div>
-                        <div className="grid gap-5 mt-2 mb-6 justify-items-center">
-                            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
-                                View Project
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div className="my-10 mx-8">
-                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
-                        <img src="https://images.pexels.com/photos/1374295/pexels-photo-1374295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="img" className="w-100" />
-                        <div className="px-6 py-4">
-                            <div className="text-white mb-2 text-2xl font-bold px-2">FFDS</div>
-                            <p className="text-white mb-3 px-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit mollitia et officiis ut vero reiciendis voluptatum iusto. Perspiciatis cupiditate aut non quos.</p>
-                        </div>
-                        <div className="grid gap-5 mt-2 mb-6 justify-items-center">
-                            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
-                                View Project
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <h2 className="text-yellow-400 sm:text-center lg:text-left lg:mx-16 text-2xl">2020</h2>
-            <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center">
-
-                <div className="my-10 mx-8">
-                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
-                        <img src="https://images.pexels.com/photos/1374295/pexels-photo-1374295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="img" className="w-100" />
-                        <div className="px-6 py-4">
-                            <div className="text-white mb-2 text-2xl font-bold px-2">FFDS</div>
-                            <p className="text-white mb-3 px-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit mollitia et officiis ut vero reiciendis voluptatum iusto. Perspiciatis cupiditate aut non quos.</p>
-                        </div>
-                        <div className="grid gap-5 mt-2 mb-6 justify-items-center">
-                            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
-                                View Project
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="my-10 mx-8">
-                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
-                        <img src="https://images.pexels.com/photos/1374295/pexels-photo-1374295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="img" className="w-100" />
-                        <div className="px-6 py-4">
-                            <div className="text-white mb-2 text-2xl font-bold px-2">FFDS</div>
-                            <p className="text-white mb-3 px-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit mollitia et officiis ut vero reiciendis voluptatum iusto. Perspiciatis cupiditate aut non quos.</p>
-                        </div>
-                        <div className="grid gap-5 mt-2 mb-6 justify-items-center">
-                            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
-                                View Project
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div className="my-10 mx-8">
-                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-gray-900">
-                        <img src="https://images.pexels.com/photos/1374295/pexels-photo-1374295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="img" className="w-100" />
-                        <div className="px-6 py-4">
-                            <div className="text-white mb-2 text-2xl font-bold px-2">FFDS</div>
-                            <p className="text-white mb-3 px-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit mollitia et officiis ut vero reiciendis voluptatum iusto. Perspiciatis cupiditate aut non quos.</p>
-                        </div>
-                        <div className="grid gap-5 mt-2 mb-6 justify-items-center">
-                            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-auto">
-                                View Project
-                            </button>
-                        </div>
-                    </div>
-                </div>
             </div>
         </section>
-
     );
-}
 
-export default Project;
+}
--- frontend/components/Secondnavbar/Navbar2.js
+++ frontend/components/Secondnavbar/Navbar2.js
@@ -5,31 +5,16 @@ const Navbar2 = () => {
     return (
 
         <div className={nav2.nav}>
-            <nav className={nav2.navbar2}>
-                <ul className={nav2.li}>
-                    <li className={nav2.items}><Link href="/"><a className={nav2.listlink}>2021</a></Link></li>
-                    <li className={nav2.items}><Link href="/"><a className={nav2.listlink}>2020</a></Link></li>
-                    <li className={nav2.items}><Link href="/"><a className={nav2.listlink}>2019</a></Link></li>
-                    <li className={nav2.items}><Link href="/"><a className={nav2.listlink}>2018</a></Link></li>
-                </ul>
-            </nav>
-        </div>
+            <ul className={nav2.items}>
+                <li className={nav2.li}><Link href=""><a >2021</a></Link></li>
+                <li className={nav2.li}><Link href=""><a >2020</a></Link></li>
+                <li className={nav2.li}><Link href=""><a >2019</a></Link></li>
+                <li className={nav2.li}><Link href=""><a >2018</a></Link></li>
 
-        // <div class="flex flex-row ...">
-        //     <ul className={nav2.li}>
-        //         <li className={nav2.items}><Link href="/"><a className={nav2.listlink}>2021</a></Link></li>
-        //         <li className={nav2.items}><Link href="/"><a className={nav2.listlink}>2020</a></Link></li>
-        //         <li className={nav2.items}><Link href="/"><a className={nav2.listlink}>2019</a></Link></li>
-        //         <li className={nav2.items}><Link href="/"><a className={nav2.listlink}>2018</a></Link></li>
-        //     </ul>
-        // </div>
-        // <div className="flex mb-4 bg-black">
-        //     <div className="w-1/4 bg-black text-white h-12"><Link href="/"><a className={nav2.listlink} className="lg:mx-20 lg:py-5">2021</a></Link></div>
-        //     <div className="w-1/4 bg-black text-white h-12"><Link href="/"><a className={nav2.listlink} className="lg:mx-20 lg:py-5">2020</a></Link></div>
-        //     <div className="w-1/4 bg-black text-white h-12"><Link href="/"><a className={nav2.listlink} className="lg:mx-20 lg:py-5">2019</a></Link></div>
-        //     <div className="w-1/4 bg-black text-white h-12"><Link href="/"><a className={nav2.listlink} className="lg:mx-20 lg:py-5">2018</a></Link></div>
-       
-        // </div>
+
+            </ul>
+
+        </div>
     );
 }
 
